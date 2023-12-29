2af6ef9c2436b1b1230258294411ef29e08c124b
creibaud <creibaud@guardiaschool.fr>
2023-12-29 11:02:57 +0100

    Add setMoves method to Bishop, Queen, and Rook classes

--- src/libs/pieces/bishop.py
+++ src/libs/pieces/bishop.py
@@ -3,3 +3,26 @@ from src.libs.pieces.piece import Piece
 class Bishop(Piece):
     def __init__(self, screen, position, color):
         super().__init__(screen, "Bishop", f"assets/pieces/{color}/bishop.png", position, color)
+
+    def setMoves(self, pieces):
+        moves = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+
+        self.possibleMoves = []
+        self.attackMoves = []
+
+        for dx, dy in moves:
+            x, y = ord(self.position[0]), int(self.position[1])
+            while 97 <= x <= 104 and 0 <= y <= 8:
+                move = chr(x + dx) + str(y + dy)
+                if self.moveIsPossible(move, pieces):
+                    self.possibleMoves.append(move)
+
+                if self.moveIsAttackable(move, pieces):
+                    self.attackMoves.append(move)
+                    break
+
+                if not self.moveIsPossible(move, pieces) and not self.moveIsAttackable(move, pieces):
+                    break
+
+                x += dx
+                y += dy
